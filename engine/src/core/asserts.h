5b698769096931dab2ae72eec099887c06987780
WaleedYaser <waleedyaser95@gmail.com>
2021-04-18 02:07:49 +0200

    add documentaion to defines, asserts, logger

--- engine/src/core/asserts.h
+++ engine/src/core/asserts.h
@@ -2,19 +2,30 @@
 
 #include "defines.h"
 
+// define/undefine based on whether you want to enable or disable assertion, most propaply you will not
+// need to disable it.
 #define MIRAI_ASSERTION_ENABLED
 
 #if defined(MIRAI_ASSERTION_ENABLED)
+    // TODO: understand and document why do we need this check?!
     #if _MSC_VER
-        #include <intrin.h>
+        // __debugbreak: causes a breakpoint in the code where the user will be prompted to run debugger.
         #define mc_debug_break() __debugbreak()
     #else
+        // __buildin_trap: not continue next instruction
         #define mc_debug_break() __builtin_trap()
     #endif
 
+    // report assertion failure by logging it, most propaply you won't need to call this function outside
+    // the macros defined bellow.
+    //  expression: string for the failed expression
+    //  message: string to log as the reason for the failure
+    //  file: file where assertion failed
+    //  line: line in the file where assertion failed
     MIRAI_API void
     mc_report_assertion_failure(const char *expression, const char *message, const char *file, i32 line);
 
+    // assert an expression in release and debug mode
     #define MC_ASSERT(expr)                                             \
     {                                                                   \
         if (!(expr))                                                    \
@@ -24,6 +35,7 @@
         }                                                               \
     }
 
+    // assert an expression and report error message in release and debug mode
     #define MC_ASSERT_MSG(expr, message)                                     \
     {                                                                        \
         if (!(expr))                                                         \
@@ -34,6 +46,7 @@
     }
 
     #if defined(MIRAI_DEBUG)
+        // assert an expression in debug mode
         #define MC_ASSERT_DEBUG(expr)                                       \
         {                                                                   \
             if (!(expr))                                                    \
@@ -42,6 +55,7 @@
                 mc_debug_break();                                           \
             }                                                               \
         }
+        // assert an expression and report error message in debug mode only
         #define MC_ASSERT_MSG_DEBUG(expr, message)                               \
         {                                                                        \
             if (!(expr))                                                         \
